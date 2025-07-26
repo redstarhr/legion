cb0827e63d5cc84e9e4e982877191d79eb5e4d4b
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-26 15:33:00 +0900

    Create configManager.js

---
+++ quest_bot/utils/configManager.js
@@ -0,0 +1,85 @@
+// quest_bot/utils/configManager.js
+const path = require('path');
+const { ensureJsonFile, readJson, writeJson } = require('./gcsFileHelper');
+
+const DEFAULT_QUEST_CONFIG = {
+  logChannelId: null,
+  notificationChannelId: null,
+  embedColor: '#00bfff',
+  buttonOrder: ['accept', 'cancel', 'edit', 'dm'],
+  dashboard: null, // { messageId: string, channelId: string }
+};
+
+function getQuestConfigPath(guildId) {
+  return `data-legion/quest_bot/${guildId}/quest_config.json`;
+}
+
+async function getQuestConfig(guildId) {
+  const path = getQuestConfigPath(guildId);
+  return await ensureJsonFile(path, DEFAULT_QUEST_CONFIG);
+}
+
+async function saveQuestConfig(guildId, updates) {
+  const path = getQuestConfigPath(guildId);
+  const current = await getQuestConfig(guildId);
+  const merged = { ...current, ...updates };
+  await writeJson(path, merged);
+  return merged;
+}
+
+// 各個別設定 getter/setter
+async function getLogChannel(guildId) {
+  const config = await getQuestConfig(guildId);
+  return config.logChannelId || null;
+}
+async function setLogChannel(guildId, channelId) {
+  return await saveQuestConfig(guildId, { logChannelId: channelId });
+}
+
+async function getNotificationChannel(guildId) {
+  const config = await getQuestConfig(guildId);
+  return config.notificationChannelId || null;
+}
+async function setNotificationChannel(guildId, channelId) {
+  return await saveQuestConfig(guildId, { notificationChannelId: channelId });
+}
+
+async function getEmbedColor(guildId) {
+  const config = await getQuestConfig(guildId);
+  return config.embedColor || '#00bfff';
+}
+async function setEmbedColor(guildId, color) {
+  return await saveQuestConfig(guildId, { embedColor: color });
+}
+
+async function getButtonOrder(guildId) {
+  const config = await getQuestConfig(guildId);
+  return config.buttonOrder || DEFAULT_QUEST_CONFIG.buttonOrder;
+}
+async function setButtonOrder(guildId, order) {
+  return await saveQuestConfig(guildId, { buttonOrder: order });
+}
+
+async function getDashboard(guildId) {
+  const config = await getQuestConfig(guildId);
+  return config.dashboard || null;
+}
+async function setDashboard(guildId, messageId, channelId) {
+  const dashboardData = (messageId && channelId) ? { messageId, channelId } : null;
+  return await saveQuestConfig(guildId, { dashboard: dashboardData });
+}
+
+module.exports = {
+  getQuestConfig,
+  saveQuestConfig,
+  getLogChannel,
+  setLogChannel,
+  getNotificationChannel,
+  setNotificationChannel,
+  getEmbedColor,
+  setEmbedColor,
+  getButtonOrder,
+  setButtonOrder,
+  getDashboard,
+  setDashboard,
+};
